80c031b39fed42ec7164b18fd7c1cbaab410a571
adam n <3193391+bcuz@users.noreply.github.com>
2024-10-02 09:22:38 -0400

    progress

--- myApp.js
+++ myApp.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World")
 
+// didnt expect to have much trouble lol
+// app.use('/public', express.static(__dirname + '/public'))
 
-
-
-
-
-
-
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
 
 
 

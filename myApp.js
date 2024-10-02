682506e84e7e3a9e8a56e1ceb2201dce2af8c7bc
adam n <3193391+bcuz@users.noreply.github.com>
2024-10-02 10:14:56 -0400

    C:/Program Files/Git/now

--- myApp.js
+++ myApp.js
@@ -7,6 +7,13 @@ app.use(function(req, res, next) {
     next();
 })
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString()
+    next();
+  }, function(req, res) {
+    res.send({time: req.time});
+})
+
 app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {

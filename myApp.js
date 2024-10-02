03d4bbebd92b494a367caff2365df2d2762a438c
adam n <3193391+bcuz@users.noreply.github.com>
2024-10-02 10:05:48 -0400

    progress

--- myApp.js
+++ myApp.js
@@ -1,17 +1,27 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next();
+})
 
-// didnt expect to have much trouble lol
-// app.use('/public', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
+app.get('/json', (req, res) => {
+    let message = "Hello json"
 
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        message = message.toUpperCase()
+    }
 
+    res.json({message: message})
+})
 
 
 
